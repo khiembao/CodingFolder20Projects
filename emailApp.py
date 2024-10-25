d43721ccde2ca70a15ba2de6ce9b3f8425517265
khiembao <wert.kb@gmail.com>
2024-10-25 15:36:35 -0400

    second_progress

--- emailApp.py
+++ emailApp.py
@@ -4,4 +4,6 @@ from email.message import EmailMessage
 email_sender = 'wert.kb@gmail.com'
 email_password = 'password'
 
-email_receiver = _
+email_receiver = 'helloworld'
+email_receiver2 = 'helloworld'
+email_receiver3 = 'helloworld'
