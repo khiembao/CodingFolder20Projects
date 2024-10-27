45a85da2fdeebe16bceaa34b133932cdc9fc7836
khiembao <wert.kb@gmail.com>
2024-10-27 12:18:12 -0400

    emailApp done

--- emailApp.py
+++ emailApp.py
@@ -1,9 +1,29 @@
 from email.message import EmailMessage
+import ssl
+import smtplib
+
 
 
 email_sender = 'wert.kb@gmail.com'
-email_password = 'password'
+email_password = '********'
 
-email_receiver = 'helloworld'
-email_receiver2 = 'helloworld'
-email_receiver3 = 'helloworld'
+email_receiver = ''
+
+subject = 'its a hello world'
+
+body = """
+When you see this message say hi back
+"""
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
+
